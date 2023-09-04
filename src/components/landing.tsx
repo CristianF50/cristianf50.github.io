dbece4f2c5f5bb01314f02e628d098c466941faa
Crisolino <cf@iseeyoutech.com>
2023-09-04 08:35:41 -0600

    Cosos

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,25 +1,39 @@
-import { Button, Layout, Row, Col } from "antd"
-import Header from "./header"
+import { Button, Layout, Row, Col, Typography } from "antd";
+import Header from "./header";
 
-import "../styles/landing.scss"
+import "../styles/landing.scss";
+
+const { Text } = Typography;
 
 function Landing() {
-    return (
-        <Layout>
-            <Header/>
-            <Layout.Content className="full-screen-intro">
-                <Row className="w-100 h-100 presentacion" align={"middle"} justify={"center"}>
-                    Hola
-                </Row>
-            </Layout.Content>
-            <Layout.Content className="full-screen-intro">
-                <Row className="w-100 h-100 presentacion" align={"middle"} justify={"center"}>
-                    Hola
-                </Row>
-                </Layout.Content>
-            
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Header />
+      <Layout.Content className="full-screen-intro">
+        <Row className="w-100 h-100" align={"middle"} justify={"center"}>
+          <Col>
+            <Row>
+              <Text className="presentacion">THIS </Text>
+              <Text className="presentacion-outline">IS </Text>
+              <Text className="presentacion">A</Text>
+            </Row>
+            <Row>
+              <Text className="presentacion">PLACEHOLDER</Text>
+            </Row>
+          </Col>
+        </Row>
+      </Layout.Content>
+      <Layout.Content className="full-screen-intro">
+        <Row
+          className="w-100 h-100 presentacion"
+          align={"middle"}
+          justify={"center"}
+        >
+          Hola
+        </Row>
+      </Layout.Content>
+    </Layout>
+  );
 }
 
-export default Landing
+export default Landing;
